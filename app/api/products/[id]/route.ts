f1269e71beb28caac19ba85a7324592913b61500
Rali <rayanedu92290@gmail.com>
2025-03-08 00:34:52 +0100

    add

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -3,12 +3,13 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
   request: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   try {
+    const { id } = await params;
     const product = await prisma.product.findUnique({
       where: {
-        id: params.id,
+        id: id,
       },
     });
     if (!product) {
