4310c9ff7b951ea841dac88f81ce2f3f86d5f275
Rali <rayanedu92290@gmail.com>
2025-03-08 00:30:32 +0100

    add

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -1,9 +1,8 @@
-// src/app/api/products/[id]/route.ts
 import { prisma } from "@/src/lib/prisma";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
-  request: Request,
+  request: NextRequest,
   { params }: { params: { id: string } }
 ) {
   try {
@@ -12,14 +11,12 @@ export async function GET(
         id: params.id,
       },
     });
-
     if (!product) {
       return NextResponse.json(
         { error: "Produit non trouvé" },
         { status: 404 }
       );
     }
-
     return NextResponse.json({ product });
   } catch (error) {
     console.error("Erreur lors de la récupération du produit:", error);
